a43d7e94a508498aadfbd12340d2c847e62fcf28
Paulo Moreira <phmoreira37@gmail.com>
2023-03-25 19:17:55 -0300

    feat: DirectSalesBanner
    Classe com função de alterar e remover banner da internade Vendas Diretas

--- layout-3.0/main.js
+++ layout-3.0/main.js
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+class DirectSalesBanner {
+  /**
+   * Create the constructor object
+   * @param {String} selector The banner selector
+   */
+  constructor(selector = '.default-bg__new_vehicle') {
+    this.banner = document.querySelector(selector);
+
+    if (!this.banner) throw new Error(`Element with selector ${selector} not found in DOM`);
+  }
+
+  /**
+   * Change the banner background-image
+   * @param {String} newBannerUrl The new banner url path
+   */
+  change(newBannerUrl) {
+    this.banner.style.backgroundImage = 'url(' + newBannerUrl + ')';
+  }
+
+  /**
+   * Remove the banner background-image
+   */
+  remove() {
+    this.banner.style.backgroundImage = 'none';
+  }
+}
