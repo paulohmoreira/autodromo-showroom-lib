88028ccb1c7ca811963f0c5a9b38ded109cf4537
Paulo Moreira <phmoreira37@gmail.com>
2023-04-01 06:01:34 -0300

    feat: Add Whatsapp Flutuante
    Cria e renderiza whatsapp flutuante sem dropdown

--- layout-3.0/main.js
+++ layout-3.0/main.js
@@ -24,3 +24,44 @@ class DirectSalesBanner {
     this.banner.style.backgroundImage = 'none';
   }
 }
+
+class WhatsappFlutuante {
+  /**
+   * Creates a new instance of WhatsappFlutuante
+   */
+  constructor() {}
+
+  /**
+   * Create the floating whatsapp element
+   * @param {String} number The whatsapp number
+   * @param {String} imagePath The URL of the Whatsapp icon image
+   * @returns {Node} The floating Whatsapp element
+   */
+  create(number, imagePath) {
+    const whatsapp = document.createElement('div');
+    whatsapp.id = 'popup-whats';
+    whatsapp.style.cssText = `
+                                position: fixed; 
+                                width: 50px; 
+                                height: 50px; 
+                                cursor: pointer; 
+                                bottom: 20px; 
+                                right: 20px; 
+                                z-index: 999;
+                                background-image: url(${imagePath})
+                              `;
+    whatsapp.setAttribute('onclick', `javascript:window.open('https://api.whatsapp.com/send?phone=${number}')`);
+    return whatsapp;
+  }
+
+  /**
+   * Add the floating whatsapp element to the page
+   * @param {String} number The whatsapp number
+   * @param {String} imagePath The URL of the Whatsapp icon image
+   */
+  add(number, imagePath = '//legado.autoforce.com.br/mods/plugins/WhatsApp/img/whatsapp-icone-7.png') {
+    const body = document.querySelector('body');
+    const whatsapp = this.create(number, imagePath);
+    body.appendChild(whatsapp);
+  }
+}
